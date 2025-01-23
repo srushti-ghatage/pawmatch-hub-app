d561847c6cd012835d9d89cd2b0393e83d35a510
Srushti Ghatage <srushtianilghatage@gmail.com>
2025-01-22 21:10:57 -0800

    added unit tests for listUtils

---
+++ src/mockData/index.ts
@@ -0,0 +1,188 @@
+export const defaultMockDogList = [
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10225.jpg",
+    name: "Yolanda",
+    age: 13,
+    breed: "Affenpinscher",
+    zip_code: "21056",
+    id: "NXGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10437.jpg",
+    name: "Wendell",
+    age: 1,
+    breed: "BullDog",
+    zip_code: "06104",
+    id: "NnGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_11365.jpg",
+    name: "Lea",
+    age: 11,
+    breed: "German Shepherd",
+    zip_code: "36032",
+    id: "RHGFTIcBOvEgQ5OCx8A1",
+  },
+];
+
+export const mockDogListByBreedAsc = [
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10225.jpg",
+    name: "Yolanda",
+    age: 13,
+    breed: "Affenpinscher",
+    zip_code: "21056",
+    id: "NXGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10437.jpg",
+    name: "Wendell",
+    age: 1,
+    breed: "BullDog",
+    zip_code: "06104",
+    id: "NnGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_11365.jpg",
+    name: "Lea",
+    age: 11,
+    breed: "German Shepherd",
+    zip_code: "36032",
+    id: "RHGFTIcBOvEgQ5OCx8A1",
+  },
+];
+
+export const mockDogListByBreedDesc = [
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_11365.jpg",
+    name: "Lea",
+    age: 11,
+    breed: "German Shepherd",
+    zip_code: "36032",
+    id: "RHGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10437.jpg",
+    name: "Wendell",
+    age: 1,
+    breed: "BullDog",
+    zip_code: "06104",
+    id: "NnGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10225.jpg",
+    name: "Yolanda",
+    age: 13,
+    breed: "Affenpinscher",
+    zip_code: "21056",
+    id: "NXGFTIcBOvEgQ5OCx8A1",
+  },
+];
+
+export const mockDogListByNameAsc = [
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_11365.jpg",
+    name: "Lea",
+    age: 11,
+    breed: "German Shepherd",
+    zip_code: "36032",
+    id: "RHGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10437.jpg",
+    name: "Wendell",
+    age: 1,
+    breed: "BullDog",
+    zip_code: "06104",
+    id: "NnGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10225.jpg",
+    name: "Yolanda",
+    age: 13,
+    breed: "Affenpinscher",
+    zip_code: "21056",
+    id: "NXGFTIcBOvEgQ5OCx8A1",
+  },
+];
+
+export const mockDogListByNameDesc = [
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10225.jpg",
+    name: "Yolanda",
+    age: 13,
+    breed: "Affenpinscher",
+    zip_code: "21056",
+    id: "NXGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10437.jpg",
+    name: "Wendell",
+    age: 1,
+    breed: "BullDog",
+    zip_code: "06104",
+    id: "NnGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_11365.jpg",
+    name: "Lea",
+    age: 11,
+    breed: "German Shepherd",
+    zip_code: "36032",
+    id: "RHGFTIcBOvEgQ5OCx8A1",
+  },
+];
+
+export const mockDogListByAgeAsc = [
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10437.jpg",
+    name: "Wendell",
+    age: 1,
+    breed: "BullDog",
+    zip_code: "06104",
+    id: "NnGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_11365.jpg",
+    name: "Lea",
+    age: 11,
+    breed: "German Shepherd",
+    zip_code: "36032",
+    id: "RHGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10225.jpg",
+    name: "Yolanda",
+    age: 13,
+    breed: "Affenpinscher",
+    zip_code: "21056",
+    id: "NXGFTIcBOvEgQ5OCx8A1",
+  },
+];
+
+export const mockDogListByAgeDesc = [
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10225.jpg",
+    name: "Yolanda",
+    age: 13,
+    breed: "Affenpinscher",
+    zip_code: "21056",
+    id: "NXGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_11365.jpg",
+    name: "Lea",
+    age: 11,
+    breed: "German Shepherd",
+    zip_code: "36032",
+    id: "RHGFTIcBOvEgQ5OCx8A1",
+  },
+  {
+    img: "https://frontend-take-home.fetch.com/dog-images/n02110627-affenpinscher/n02110627_10437.jpg",
+    name: "Wendell",
+    age: 1,
+    breed: "BullDog",
+    zip_code: "06104",
+    id: "NnGFTIcBOvEgQ5OCx8A1",
+  },
+];
